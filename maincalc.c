8f32ce9fe7ea7fcf40f6b1664f64b1b72d2f88aa
Guilherme <guilherme.silva2616@etec.sp.gov.br>
2023-10-31 11:47:43 -0300

    31-10

--- maincalc.c
+++ maincalc.c
@@ -8,45 +8,64 @@ int main(int argc, char *argv[]) {
 	int op; /* Diz qual a intenção para o usuario */
 	float n1, n2, res; /*variaveis de valores reais*/
 	
+	op=1;
+	while(op!=0){ /* Enquanto operação for diferente de 0... */
+	
 	/*As opções para o usuario */
+	printf("Calculadora. \n");
+	
 	printf("Digite 1 para somar;\n"); 
-	printf("Digite 2 para subtrair;\n");
-	printf("Digite 3 para multiplicar;\n");
-	printf("Digite 4 para dividir;\n");
+	printf("Digite 2 para subtrair.\n");
+	printf("Digite 3 para multiplicar.\n");
+	printf("Digite 4 para dividir.\n");
+	printf("Digite 0 para sair.\n");
 	scanf("%d", &op); /*Será amrazenado a opção da intenção do usuario */
+	
+	if (op==0){ /*Se a operação for igual a 0*/
+		
+	}else{ /* Se não...*/
 	
 	printf("Insira o primeiro numero: \n"); /* Mensagem para o usuario*/
 	scanf("%f", &n1); /* Salva a infomação dada pelo usuario das OP */
 	
 	printf("Insira o segundo numero: \n");
 	scanf("%f", &n2);
+}
 	
 	switch(op){ /* Cria condições para as situações*/
-	case 1: /* Significa  sobre a OPÇÃO SOMAR*/
+	
+	case 1: /* Situação sobre a OPÇÃO SOMAR*/
 		res = n1 + n2;
 		printf("Soma: %f.\n", res);
 		break;
 	
-	case 2:
+	case 2: /*Situação sobre a opção SUBTRAIR */
 		res = n1 - n2;
 		printf("Subtracao: %f.\n", res);
 		break;
 		
-	case 3:
+	case 3: /*Situação sobre a opção MULTIPLICAR */
 		res = n1 * n2;
 		printf("Multiplicacao: %f.\n", res);
 		break;
 		
-	case 4:
+	case 4: /*Situação sobre a opção DIVIDIR */
 		
-		if(n2 !=0){ /* serve no caso de "SE" n2 for "diferente" de 0*...*/
-			res = n1 / n2;
-		printf("Divisao: %f.\n", res); /* ... Vai aparecer o res certo*/
-	} else { /* SE não for...*/
-	
-		printf("Divisao por zero.\n");
-	}
+		while(n2 ==0){
+		 printf("Operacao invalida\n");
+		 printf("Digite outro numero\n");
+		 scanf("%f", &n2);
 }
-
+		res = n1 / n2;
+		printf("Divisao: %.2f.\n", res); /* ... Vai aparecer o res certo*/
+		break;
+		
+	case 0:
+		printf("Aperte Enter pra a Sain...");
+		exit(0);
+		break;
+	
 	return 0;
 } 
+}
+}
